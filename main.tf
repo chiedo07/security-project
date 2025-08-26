e989e558b5fdbb4e259d0b1a3abab72aa21a179a
chiedo07 <chiedoluiz@gmail.com>
2025-08-26 18:12:12 +0100

    first push

---
+++ main.tf
@@ -0,0 +1,125 @@
+resource "azurem_resource_group" "sec" {
+    name = var.azurem_resource_group_name
+    location = var.location
+    tags = var.tags
+}
+
+#Virtual Networks
+
+resource "azurerem_virtual_network" "net" {
+  name = "${var.project_name} -vnet"
+  address_space = ["10.0.0.0/16"]
+  location = azurem_resource_group.sec.location
+  resource_group_name = azurem_resource_group.sec.name
+}
+
+#Subnets
+
+resource "azurem_subnet" "web-tier" {
+  name                 = "web-tier-subnet"
+  resource_group_name  = azurerm_resource_group.sec.name
+  virtual_network_name = azurerm_virtual_network.sec.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location = azurem_resource_group.sec.location
+  resource_group_name = azurem_resource_group.sec.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web-tier.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "secure" {
+  name                = "security-machine"
+  resource_group_name = azurerm_resource_group.sec.name
+  location            = azurerm_resource_group.sec.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+# Web Tier NSG
+
+resource "azurerm_network_security_group" "web_tier" {
+  name                = "${var.project_name}-web-tier-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  # Allow HTTP traffic
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTPS traffic
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow SSH from management subnet only
+  security_rule {
+    name                       = "AllowSSHFromManagement"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.4.0/24"
+    destination_address_prefix = "*"
+  }
+
+  # Deny all other inbound traffic
+  security_rule {
+    name                       = "DenyAllInbound"
+    priority                   = 4000
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = var.tags
+}
---
+++ var.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "sec-rg"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "project_name" {
+  description = "security-project"
+  type        = string
+}
+
+variable "tags" {
+  description = "sec-tag"
+  type        = map(string)
+  default = {
+    Project     = "Azure-Security-Demo"
+    Environment = "Development"
+  }
+}
